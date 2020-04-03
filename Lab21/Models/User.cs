9c601ebfddca2ca625a091f3ad0eeddda8c1c3f4
Jake Collins <jakecollins1014@gmail.com>
2020-04-03 05:11:57 -0400

    working on validation

--- Lab21/Models/User.cs
+++ Lab21/Models/User.cs
@@ -1,9 +1,18 @@
-﻿namespace Lab21.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab21.Models
 {
     public class User
     {
         public string UserName { get; set; }
+        [Required(ErrorMessage = "A username is required.")]
         public string PassWord { get; set; }
+        [Required(ErrorMessage = "A password is required.")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "An email is required.")]
+        [RegularExpression("^([a - zA - Z0 - 9_\\-\\.]+)@([a - zA - Z0 - 9_\\-\\.]+)\\.([a - zA - Z]{2,5})$", ErrorMessage = "Please input a valid number")]
+        public string PhoneNum { get; set; }
+        [Required]
+        [RegularExpression("^(\\(?\\+?[0-9]*\\)?)?[0-9_\\- \\(\\)]*$", ErrorMessage = "Please input a valid number")]
     }
 }
